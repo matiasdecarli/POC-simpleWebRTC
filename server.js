0fcb1dc832d17552210b6b845c2270ab6195df53
matiasdecarli <matiasdecarli@gmail.com>
2014-08-11 16:31:02 -0300

    changes to use our signaling server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 5000;
+var port = 8888;//process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
 app.listen(port);   
